13d9f85abad5c78f14d0469fd19aa18234f93a7e
Minyeong Jeong <megormin@gmail.com>
2024-11-04 14:51:21 +0100

    html changed und ts

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,46 @@
-import './style.css'
+import "./style.css";
+
+const five = document.querySelector("#five") as HTMLInputElement;
+const ten = document.querySelector("#ten") as HTMLInputElement;
+const fifteen = document.querySelector("#fifteen") as HTMLInputElement;
+const twenty = document.querySelector("#twenty") as HTMLInputElement;
+
+const rock = document.querySelector("#rock") as HTMLDivElement;
+const paper = document.querySelector("#paper") as HTMLDivElement;
+const scissors = document.querySelector("#scissors") as HTMLDivElement;
+
+const result = document.querySelector("#result") as HTMLParagraphElement;
+
+enum gameInput {
+  rock,
+  paper,
+  scissors,
+}
+
+let youInput: gameInput;
+let computerInput: gameInput;
+computerInput = Math.floor(Math.random() * 3);
+console.log(gameInput[computerInput]);
+
+rock?.addEventListener("click", () => {
+  youInput = 0;
+  console.log(youInput);
+
+  return youInput;
+});
+paper?.addEventListener("click", () => {
+  youInput = 1;
+  console.log(youInput);
+
+  return youInput;
+});
+scissors?.addEventListener("click", () => {
+  youInput = 2;
+  console.log(youInput);
+
+  return youInput;
+});
+
+// switch(){
+//     case
+// }
